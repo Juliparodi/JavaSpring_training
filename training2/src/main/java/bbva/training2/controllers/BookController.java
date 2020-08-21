7870a6f945642a9e7bb5b40992542a45652e7424
Parodi <A128776@bf.arg.igrupobbva>
2020-08-21 17:24:53 -0300

    lala

--- training2/src/main/java/bbva/training2/controllers/BookController.java
+++ training2/src/main/java/bbva/training2/controllers/BookController.java
@@ -1,20 +1,17 @@
-
 package bbva.training2.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class BookRepository {
+@Controller
+public class BookController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "greeting.html";
     }
+
 }
-
-
